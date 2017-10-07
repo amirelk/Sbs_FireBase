43605a461afe2aef7f7e2fae35bdcc573c4bde04
Amir Elkayam <amir.elk116@gmail.com>
2017-10-08 01:09:35 +0300

    Changes: add new Classes

--- app/src/main/java/com/test/amirelkayam/sbs_firebase/Customers.java
+++ app/src/main/java/com/test/amirelkayam/sbs_firebase/Customers.java
@@ -3,19 +3,39 @@ package com.test.amirelkayam.sbs_firebase;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.jar.Attributes;
 
 public class Customers extends AppCompatActivity {
+
+    private FirebaseAuth auth;                              // reference to the users
+    private DatabaseReference mDatabaseCustomers;           // reference to the data in the DB
+
+
+    //get current user
+    FirebaseUser user = auth.getInstance().getCurrentUser();
+
 
     private ImageView mMainImage;
     private EditText mFindCustomers;
     private Button mButtonFindCustomers;
     private Button mButtonNewCustomer;
     private Button btnBack;
-
 
 
     @Override
@@ -28,8 +48,6 @@ public class Customers extends AppCompatActivity {
         mButtonFindCustomers = (Button)findViewById(R.id.btn_findCustomers);
         mButtonNewCustomer = (Button)findViewById(R.id.btn_newCustomer);
         btnBack = (Button) findViewById(R.id.btn_back);
-
-
 
         mMainImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +63,54 @@ public class Customers extends AppCompatActivity {
             }
         });
 
+        mDatabaseCustomers = FirebaseDatabase.getInstance().getReference();
+
+        //// ReycyclerView Customers
+        RecyclerView recyclerView_Customer = (RecyclerView) findViewById(R.id.rv_Customer);
+
+        FirebaseRecyclerAdapter <String, Customers.CustomerViewHolder> adapter_customer;
+
+        recyclerView_Customer.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView_Customer.setHasFixedSize(true);
+
+        adapter_customer = new FirebaseRecyclerAdapter<String, Customers.CustomerViewHolder>(
+                String.class,
+                R.layout.customers_rv_item,
+                Customers.CustomerViewHolder.class,
+                mDatabaseCustomers.child("Users").child(user.getUid()).child("Customers").getRef().child("Name")
+        ) {
+            @Override
+            protected void populateViewHolder(final CustomerViewHolder viewHolder, String name,final int position) {      /// title = name
+                viewHolder.customerName_filed.setText(name);
+                viewHolder.delCustomer.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DatabaseReference itemRef = getRef(position);
+                        itemRef.removeValue();
+
+                    }
+                });
+            }
+        };
+        recyclerView_Customer.setAdapter(adapter_customer);
+
+
+
     }    // End OnCreate
+
+
+    private static class CustomerViewHolder extends RecyclerView.ViewHolder {
+
+        TextView customerName_filed;
+        Button delCustomer;
+
+        public CustomerViewHolder(View itemView) {// S CustomerViewHolder
+            super(itemView);
+            customerName_filed = (TextView) itemView.findViewById(R.id.tv_customer_name);
+            delCustomer = (Button) itemView.findViewById(R.id.btn_del_customer);
+        }
+
+    }
 
 
     // OnClick NewCustomer from customers XML
